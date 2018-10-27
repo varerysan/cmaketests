8146a794382db46a19b95235e6c520c041698ac3
Valery Mosyagin <valery.mosyagin@gmail.com>
2018-10-28 02:55:20 +0300

    Some tests

--- modules/core/tests/test_main.cpp
+++ modules/core/tests/test_main.cpp
@@ -26,6 +26,20 @@ TEST(Simple_test_fail2, test_sum)
 int main(int argc, char *argv[])
 {
     std::cout << "Pointer size=" << sizeof(int*) << std::endl;
+
+#if DEBUG
+    std::cout << "DEBUG is set" << std::endl;
+#else
+    std::cout << "DEBUG is not set" << std::endl;
+#endif
+
+#if RELEASE
+    std::cout << "RELEASE is set" << std::endl;
+#else
+    std::cout << "RELEASE is not set" << std::endl;
+#endif
+
+
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 //    std::cout << "Hello world" << std::endl;
